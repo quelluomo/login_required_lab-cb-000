983b63b6b55772babff2151cd14d46139ca4248d
Matteo Muscas <thatman.j@gmail.com>
2019-07-05 09:24:11 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,14 +2,12 @@ class SecretsController < ApplicationController
   before_action :require_login
   skip_before_action :require_login, only: [:index]
 
-  
+
 
   private
 
   def require_login
     return head(:forbidden) unless session.include? :user_id
   end
-
-end
 
 end
